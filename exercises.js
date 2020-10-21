4f4621d7b58a2356acba33a72a7f8415ea915205
sschneeberg <simone.m.schneeberg@gmail.com>
2020-10-21 15:36:56 -0400

    [exercises] Add file to project

---
+++ exercises.js
@@ -0,0 +1,22 @@
+const iPhone = {
+    image: 'image location',
+    videoLength: '11:28',
+    title: 'iPhone 12 Unboxing Experience + MagSafe Demo',
+    username: 'Marques Brownlee',
+    views: '5.5M views',
+    uploaded: '15 hours ago'
+}
+
+console.log(iPhone.title);
+
+const starWars = {
+    image: 'image location',
+    videoLength: '1:00',
+    title: 'Special Look | The Mandalorian | Disney +',
+    username: 'Start Wars',
+    views: '1M views',
+    uploaded: '1 day ago'
+}
+
+console.log(starWars.views);
+console.log(Object.keys(starWars));
